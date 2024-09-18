13df405c9081ece6f23474245a6a9654b0ca072f
Jugal Kishore <me@devjugal.com>
2024-09-19 02:00:36 +0530

    Update
    .github/workflows/actions.yml
    generate.sh

--- generate.sh
+++ generate.sh
@@ -2,9 +2,6 @@
 #
 # Generates "README.md" with IMDb top 50 movies
 #
-# Variables
-DATE="$(date +%m/%d/%y)"
-GH_REF="github.com/crazyuploader/IMDb_Top_50.git"
 
 # Colors
 NC="\033[0m"
@@ -13,12 +10,6 @@ GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 
 # Main Script
-git remote remove origin
-git remote add origin https://"${GH_REF}"
-git fetch --all
-echo ""
-# echo -e "# Top IMDb 50 Movies Data Scrapper\n" > README.md
-# echo -e "Top 50 Movies as of: **$(date +%m/%d/%Y)**\n" >> README.md
 ./IMDb.py 1> /dev/null
 ERROR_CODE="$?"
 if [[ "${ERROR_CODE}" != "0" ]]; then
@@ -34,22 +25,13 @@ cd data/top50 || exit 1
 csvtojson movies.csv > movies.json
 csvtojson shows.csv > shows.json
 echo ""
-cd ..
-cd top250 || exit 1
+cd "${GITHUB_WORKSPACE}" || exit 1
+cd data/top250 || exit 1
 csvtojson movies.csv > movies.json
 echo ""
-cd "${GITHUB_WORKSPACE}" || exit 1
-echo "Prettify..."
-prettier --write .
 if [[ -z $(git status --porcelain) ]]; then
     echo -e "${GREEN}Nothing to Update${NC}"
 else
     echo -e "${YELLOW}File(s) changed -${NC}"
     git diff --name-only
-    git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
-    git config user.name "github-actions"
-    git add .
-    git commit -m "CI ${DATE} [skip ci]"
-    git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GH_REF}" HEAD:"${GITHUB_REF}"
-    echo -e "${YELLOW}Changes pushed to${NC} ${GREEN}'https://github.com/crazyuploader/IMDb_Top_50'${NC}"
 fi
