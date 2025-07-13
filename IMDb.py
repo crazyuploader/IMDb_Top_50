f8dc9f85bebc209c6fb73582726b31d463dd02ae
Jugal Kishore <me@devjugal.com>
2025-07-13 10:33:21 +0530

    Update User Agent in IMDb.py

--- IMDb.py
+++ IMDb.py
@@ -34,7 +34,7 @@ IMDB_POPULAR_TV_URL = "https://www.imdb.com/chart/tvmeter/"
 
 # Custom headers
 HEADERS = {
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0"
 }
 
 
